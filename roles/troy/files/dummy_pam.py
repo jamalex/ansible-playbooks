515e8836f3d869d6111418090d5706dd0895399d
Jamie Alexandre <jamalex@gmail.com>
2014-04-19 16:29:18 -0700

    In-progress configuration of troy (SSH tunnel proxy server).

---
+++ roles/troy/files/dummy_pam.py
@@ -0,0 +1,35 @@
+#
+# Duplicates pam_permit.c
+#
+
+import random
+
+DEFAULT_USER    = "nobody"
+
+def pam_sm_authenticate(pamh, flags, argv):
+  # try:
+  #   user = pamh.get_user(None)
+  # except pamh.exception, e:
+  #   return e.pam_result
+  # if user == None:
+  pamh.user = DEFAULT_USER
+  if random.random() > 0.5:
+    return pamh.PAM_SUCCESS
+  else:
+    return pamh.PAM_ABORT
+
+def pam_sm_setcred(pamh, flags, argv):
+  return pamh.PAM_SUCCESS
+
+def pam_sm_acct_mgmt(pamh, flags, argv):
+  return pamh.PAM_SUCCESS
+
+def pam_sm_open_session(pamh, flags, argv):
+  return pamh.PAM_SUCCESS
+
+def pam_sm_close_session(pamh, flags, argv):
+  return pamh.PAM_SUCCESS
+
+def pam_sm_chauthtok(pamh, flags, argv):
+  return pamh.PAM_SUCCESS
+
